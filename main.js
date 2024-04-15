b4becc5f645182b97f6186e3da5a7aaa16e5ae31
Yogesh Manni <yogeshmanni786@gmail.com>
2024-04-14 22:08:04 -0700

    Correct BEM naming in homepage

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 /****  Fade and add hover effect *****/
 const testimonialPage = document.querySelector(
-  ".l-quotes-slide[data-slider-slide-index='1']"
+  ".l-quotes__slide[data-slider-slide-index='1']"
 );
-testimonialPage.classList.add("l-quotes-slide--active");
+testimonialPage.classList.add("l-quotes__slide--active");
 
 document.querySelector(".l-quotes").addEventListener("mouseout", function () {
   document
-    .querySelector(".l-quotes-slide--active")
-    .classList.remove("l-quotes-slide--active");
+    .querySelector(".l-quotes__slide--active")
+    .classList.remove("l-quotes__slide--active");
   document
-    .querySelector(".l-quotes-slide[data-slider-slide-index='1']")
-    .classList.add("l-quotes-slide--active");
+    .querySelector(".l-quotes__slide[data-slider-slide-index='1']")
+    .classList.add("l-quotes__slide--active");
 });
 
-document.querySelectorAll(".l-quotes-slide").forEach((elem) =>
+document.querySelectorAll(".l-quotes__slide").forEach((elem) =>
   elem.addEventListener("mouseover", function () {
     document
-      .querySelector(".l-quotes-slide--active")
-      .classList.remove("l-quotes-slide--active");
-    this.classList.add("l-quotes-slide--active");
+      .querySelector(".l-quotes__slide--active")
+      .classList.remove("l-quotes__slide--active");
+    this.classList.add("l-quotes__slide--active");
   })
 );
